3945afefb2258f9414ff193ff1aaced67b0c5bf8
KimRautiainen <kim.rautiainen@hotmail.com>
2023-12-14 01:30:23 +0200

    sql script WiP

--- database/controllers/userController.js
+++ database/controllers/userController.js
@@ -1,179 +1,113 @@
-'use strict';
+"use strict";
 // userController
-const userModel = require('../models/userModel');
-const bcrypt = require('bcryptjs');
+const userModel = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 const getUserList = async (req, res) => {
-    try {
-        const users = await userModel.getAllUsers();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const users = await userModel.getAllUsers();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
+const getUser = async (req, res) => {
+  const userId = Number(req.params.userId);
+  if (!Number.isInteger(userId)) {
+    res.status(400).json({ error: 500, message: "invalid id" });
+    return;
+  }
 
-const getUser =  async (req, res) => {
-    const userId = Number(req.params.userId);
-    if(!Number.isInteger(userId)) {
-        res.status(400).json({error: 500, message: 'invalid id'});
-        return;
-    }
+  const [user] = await userModel.getUserById(userId);
+  console.log("getUser", user);
 
-    const [user] = await userModel.getUserById(userId);
-    console.log('getUser', user);
-
-    if(user) {
-        res.json(user);
-    } else {
-        res.status(404).json({message: "User not found."})
-    }
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found." });
+  }
 };
-const postUser = async (req,res) => {
-    console.log("posting user", req.body, req.file);
-    // Generate a salt with bcrypt
+const postUser = async (req, res) => {
+  console.log("posting user", req.body, req.file);
+  // Generate a salt with bcrypt
+  const salt = await bcrypt.genSalt(10);
+  // Hash the user's password using bcrypt and the generated salt
+  const password = await bcrypt.hash(req.body.password, salt);
+  // Create a new user object with the user's input values and the generated password and filename
+  const newUser = {
+    name: req.body.name,
+    surname: req.body.surname,
+    email: req.body.email,
+    profession: req.body.profession,
+    description: req.body.description,
+    filename: req.file.filename,
+    password: password,
+  };
+  try {
+    // Call the insertUser method on the userModel object with the new user object as the argument and store the result
+    const result = await userModel.insertUser(newUser);
+    // Redirect the user to the registration.html page
+    res.redirect("/registration.html");
+  } catch (error) {
+    // If there is an error, log the error message and send a 500 status code and error message in JSON format to the client
+    console.error("error", error.message);
+    res.status(500).json({ error: 500, message: error.message });
+  }
+};
+const putUser = async (req, res) => {
+  try {
+    const userId = req.body.id;
     const salt = await bcrypt.genSalt(10);
-    // Hash the user's password using bcrypt and the generated salt
     const password = await bcrypt.hash(req.body.password, salt);
-    // Create a new user object with the user's input values and the generated password and filename
-    const newUser = {
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        profession: req.body.profession,
-        description: req.body.description,
-        filename: req.file.filename,
-        password: password,
-
+    console.log("userid: " + userId);
+    console.log(req.body);
+    console.log(req.body.filename);
+    const user = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      profession: req.body.profession,
+      description: req.body.description,
+      password: password,
     };
-    try {
-        // Call the insertUser method on the userModel object with the new user object as the argument and store the result
-        const result = await userModel.insertUser(newUser);
-        // Redirect the user to the registration.html page
-        res.redirect("/registration.html");
-    }catch (error){
-        // If there is an error, log the error message and send a 500 status code and error message in JSON format to the client
-        console.error("error",error.message);
-        res.status(500).json({error: 500, message: error.message});
+    if (req.file) {
+      user.filename = req.file.filename;
+    } else {
+      user.filename = req.body.sessionuser;
     }
 
-};
-const putUser = async (req, res) => {
-    try {
-        const userId = req.body.id
-        const salt = await bcrypt.genSalt(10);
-        const password = await bcrypt.hash(req.body.password, salt);
-        console.log("userid: " + userId);
-        console.log(req.body)
-        console.log(req.body.filename)
-        const user = {
-            name: req.body.name,
-            surname: req.body.surname,
-            email: req.body.email,
-            profession: req.body.profession,
-            description: req.body.description,
-            password: password,
-
-        };
-        if (req.file) {
-            user.filename = req.file.filename;
-        }else {
-            user.filename = req.body.sessionuser;
-        }
-
-        console.log("user: " + JSON.stringify(user));
-        const result = await userModel.modifyUser(userId, user);
-        res.status(200).json({ message: "User modified" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal server error" });
-    }
+    console.log("user: " + JSON.stringify(user));
+    const result = await userModel.modifyUser(userId, user);
+    res.status(200).json({ message: "User modified" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-/*
-const putUser = async (req, res) => {
-    try {
-        // Get the user ID from the request parameters
-        const userId  = req.body.id;
-
-        // Get the authenticated user object from req.user
-        const user = req.user;
-
-
-        // Check if the user exists
-        if (!user) {
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-
-        // Call your update user function and pass the authenticated user object
-        const result = await userModel.modifyUser(userId, data, user)
-        console.log("UserID: " + userId);
-        console.log("RequestBody: " + req.body);
-        console.log("request.user: " + user);
-
-        // Return the updated user object
-        return res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+const deleteUser = async (req, res) => {
+  try {
+    const result = await userModel.deleteUser(req.params.userId);
+    console.log(req.params);
+    console.log(req.params.userId);
+    res.status(200).json({ message: "User deleted" });
+  } catch (e) {
+    console.error("error", e.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
-*/
-/*
-const putUser = async (req, res) => {
-    try {
-        const userId = req.body.id;
-        console.log("userid: " + userId);
-        const user = req.body.data;
-
-        console.log(user);
-        console.log(req.body);
-        const result = await userModel.modifyUser(userId, user);
-        res.status(200).json({ message: "User modified" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-};
-
- */
-/*
-const putUser = async (req,res) => {
-
-
-    const user = req.body;
-    console.log("request body is controller" + user);
-    try {
-        const result = await userModel.modifyUser(user);
-        res.status(200).json({message: "user modified"});
-    }
-    catch (e){
-        console.error("error", e.message);
-        res.status(500).json({error: 500, message: e.message});
-    }
-
-}
-
-
- */
-
-const deleteUser = async (req,res) => {
-
-    try {
-
-        const result = await userModel.deleteUser(req.params.userId);
-        console.log(req.params);
-        console.log(req.params.userId);
-        res.status(200).json({ message: "User deleted" });
-    }catch (e){
-        console.error("error",e.message);
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
 
 const checkToken = (req, res) => {
-    res.json({user: req.user});
+  res.json({ user: req.user });
 };
 
-const userController = {getUserList, getUser, postUser, putUser, deleteUser, checkToken};
+const userController = {
+  getUserList,
+  getUser,
+  postUser,
+  putUser,
+  deleteUser,
+  checkToken,
+};
 module.exports = userController;
--- database/webDb-starter.sql
+++ database/webDb-starter.sql
@@ -1,116 +1,47 @@
-CREATE DATABASE webDb;
-USE webDb
-CREATE TABLE `Työntekijä`
+CREATE DATABASE triviaDb;
+-- Create the User table with additional columns for experience points and level
+CREATE TABLE `User`
 (
-  `tyontekija_id` INT NOT NULL,
-  `etunimi` text NOT NULL,
-  `sukunimi` text NOT NULL,
-  `sähköposti` text NOT NULL,
-  `ammatti` text NOT NULL,
-  `kuvaus` text NOT NULL,
-  `filename` text NOT NULL,
-  `salasana` text NOT NULL,
-  PRIMARY KEY (`tyontekija_id`)
+  `userId` INT NOT NULL AUTO_INCREMENT,
+  `firstName` TEXT NOT NULL,
+  `lastName` TEXT NOT NULL,
+  `email` TEXT NOT NULL,
+  `description` TEXT NOT NULL,
+  `userAvatar` TEXT NOT NULL,
+  `password` TEXT NOT NULL,
+  `experiencePoints` INT DEFAULT 0,
+  `level` INT DEFAULT 1,
+  PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `Työntekijä`
-  MODIFY `tyontekija_id` int(11) NOT NULL AUTO_INCREMENT;
-
-CREATE TABLE `Työnantaja`
-(
-  `y-tunnus` INT NOT NULL,
-  `nimi` text NOT NULL,
-  `sähköposti` text NOT NULL,
-  `kuvaus` text NOT NULL,
-  `filename` text NOT NULL,
-  `salasana` text NOT NULL,
-  `ammatti` text NOT NULL,
-  PRIMARY KEY (`Y-tunnus`)
+-- Create the Levels table
+CREATE TABLE `Level` (
+  `levelId` INT NOT NULL AUTO_INCREMENT,
+  `xpThreshold` INT NOT NULL,
+  PRIMARY KEY (`levelId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `Ammatti`
-(
-  `ammatti_id` INT NOT NULL,
-  `nimike` text NOT NULL,
-  PRIMARY KEY (`ammatti_id`)
+-------------------------------------------------------------
+--Achievements
+
+-- Create the Achievements table
+CREATE TABLE `Achievement` (
+  `achievementId` INT NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  `description` TEXT NOT NULL,
+  `icon` TEXT,
+  PRIMARY KEY (`achievementId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `Ammatti`
-  MODIFY `ammatti_id` int(11) NOT NULL AUTO_INCREMENT;
-
-CREATE TABLE `MEDIA`
-(
-  `media_id` INT NOT NULL,
-  `tiedostonimi` text NOT NULL,
-  `tyontekija_id` INT ,
-  `y-tunnus` INT ,
-  PRIMARY KEY (`media_id`),
-  FOREIGN KEY (`tyontekija_id`) REFERENCES `Työntekijä`(`tyontekija_id`),
-  FOREIGN KEY (`y-tunnus`) REFERENCES `Työnantaja`(`y-tunnus`)
+-- Create a junction table for User-Achievement relationship
+CREATE TABLE `UserAchievement` (
+  `userId` INT NOT NULL,
+  `achievementId` INT NOT NULL,
+  `dateEarned` DATETIME NOT NULL,
+  PRIMARY KEY (`userId`, `achievementId`),
+  FOREIGN KEY (`userId`) REFERENCES `User` (`userId`),
+  FOREIGN KEY (`achievementId`) REFERENCES `Achievement` (`achievementId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `MEDIA`
-  MODIFY `media_id` int(11) NOT NULL AUTO_INCREMENT;
 
-CREATE TABLE `Pyyhkäsy`
-(
-  `pyyhkäsySuunta-TT` INT NOT NULL,
-  `pyyhkäsySuunta-TA` INT NOT NULL,
-  `timestamp` INT NOT NULL,
-  `tyontekija_id` INT NOT NULL,
-  `y-tunnus` INT NOT NULL,
-  PRIMARY KEY (`tyontekija_id`, `y-tunnus`),
-  FOREIGN KEY (`tyontekija_id`) REFERENCES `Työntekijä`(`tyontekija_id`),
-  FOREIGN KEY (`y-tunnus`) REFERENCES `Työnantaja`(`y-tunnus`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `onAmmattia`
-(
-  `ammatti_id` INT NOT NULL,
-  `tyontekija_id` INT NOT NULL,
-  PRIMARY KEY (`ammatti_id`, `tyontekija_id`),
-  FOREIGN KEY (`ammatti_id`) REFERENCES `Ammatti`(`ammatti_id`),
-  FOREIGN KEY (`tyontekija_id`) REFERENCES `Työntekijä`(`tyontekija_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `EtsiiAmmattilaista`
-(
-  `ammatti_id` INT NOT NULL,
-  `y-tunnus` INT NOT NULL,
-  PRIMARY KEY (`ammatti_id`, `y-tunnus`),
-  FOREIGN KEY (`ammatti_id`) REFERENCES `Ammatti`(`ammatti_id`),
-  FOREIGN KEY (`y-tunnus`) REFERENCES `Työnantaja`(`y-tunnus`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `Viesti`
-(
-  `aikaleima` INT NOT NULL,
-  `otsake` INT NOT NULL,
-  `sisalto` text NOT NULL,
-  `viesti_id` INT NOT NULL,
-  `tyontekija_id` INT NOT NULL,
-  `y-tunnus` INT NOT NULL,
-  PRIMARY KEY (`viesti_id`),
-  FOREIGN KEY (`tyontekija_id`) REFERENCES `Työntekijä`(`tyontekija_id`),
-  FOREIGN KEY (`y-tunnus`) REFERENCES `Työnantaja`(`y-tunnus`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `Viesti`
-  MODIFY `viesti_id` int(11) NOT NULL AUTO_INCREMENT;
-
-CREATE TABLE `Vastaus`
-(
-  `aikaleima`INT NOT NULL,
-  `sisalto` text NOT NULL,
-  `vastaa_id` INT NOT NULL,
-  `viesti_id` INT NOT NULL,
-  `tyontekija_id` INT NOT NULL,
-  `y-tunnus` INT NOT NULL,
-  PRIMARY KEY (`vastaa_id`),
-  FOREIGN KEY (`viesti_id`) REFERENCES `Viesti`(`viesti_id`),
-  FOREIGN KEY (`tyontekija_id`) REFERENCES `Työntekijä`(`tyontekija_id`),
-  FOREIGN KEY (`y-tunnus`) REFERENCES `Työnantaja`(`y-tunnus`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `Vastaus`
-  MODIFY `vastaa_id` int(11) NOT NULL AUTO_INCREMENT;
+--------------------------------------------------------------
